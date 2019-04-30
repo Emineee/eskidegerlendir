636b39cb9fe3ccc1ef35eb034fff12ba00c65f60
Emineee <eminemeydanoglu@gmail.com>
2019-04-30 17:55:44 +0300

    Slider yapımı

---
+++ slider.php
@@ -0,0 +1,30 @@
+
+<div role="main" class="main">
+	<div class="slider-container rev_slider_wrapper" style="height:420px;">
+					<div id="revolutionSlider" class="slider rev_slider manual">
+							<ul>
+							<li data-transition="fade" data-title="Advocate Team" data-thumb="img/yildiz.jpg">
+
+								<img src="img/yildiz.jpg"  
+									alt=""
+									data-bgposition="center center" 
+									data-bgfit="cover" 
+									data-bgrepeat="no-repeat"
+									class="rev-slidebg">							
+							</li>
+							
+
+
+
+						</ul>
+					</div>
+				</div>
+
+
+
+
+
+
+
+
+
